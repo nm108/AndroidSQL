289cda35c189235f5429955f2bee685b17b8dff2
nm108 <neomahakala108@gmail.com>
2023-05-19 16:01:49 +0200

    Preparing to use second activity where remote database connection is configured (ip, port, dbinstance, database).
    (for now working on UI).
    
    for now code quality is very poor.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,11 +20,14 @@ public class MainActivity extends AppCompatActivity {
     private Button doQueryButton;
     private EditText userNameEditText;
 
+    private Button switchActivitiesButton;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        doQueryButton = findViewById(R.id.Btn);
-        userNameEditText = findViewById(R.id.Eb);
+        doQueryButton = findViewById(R.id.DoQueryButton);
+        userNameEditText = findViewById(R.id.UserNameEditText);
+        switchActivitiesButton = findViewById(R.id.SwitchActivitiesButton);
 
         doQueryButton.setOnClickListener(
             (final View v) -> {
@@ -27,5 +35,20 @@ public class MainActivity extends AppCompatActivity {
                 new QueryForUserDataAsyncTask(MainActivity.this, userName).execute();
             }
         );
+
+        switchActivitiesButton.setOnClickListener(
+                (final View v) -> {
+                    switchActivities();
+                }
+
+        );
+
+
+    }
+
+
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, SecondaryActivity.class);
+        startActivity(switchActivityIntent);
     }
 }
---
+++ app/src/main/java/com/example/myapplication/SecondaryActivity.java
@@ -0,0 +1,62 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+/**
+ * Entry point for application.
+ */
+public class SecondaryActivity extends AppCompatActivity {
+
+    private Button switchActivitiesButton;
+
+    private ListView lv;
+
+    private String[] myStringArray = new String[] {
+            "1", "2", "Three"
+    };
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+        switchActivitiesButton = findViewById(R.id.SwitchActivitiesButton);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, myStringArray);
+
+        ListView lv = (ListView) findViewById(R.id.SimpleListView);
+
+        lv.setAdapter(adapter);
+
+        // Create a message handling object as an anonymous class.
+        AdapterView.OnItemClickListener messageClickedHandler = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                Log.d("click", "onItemClick: ");
+            }
+        };
+
+        lv.setOnItemClickListener(messageClickedHandler);
+
+
+        switchActivitiesButton.setOnClickListener(
+                (final View v) -> {
+                    switchActivities();
+                });
+    }
+
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(switchActivityIntent);
+    }
+}
