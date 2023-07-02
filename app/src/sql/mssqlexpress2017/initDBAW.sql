75355ab0d7452f484340762639ef3746c1d7bc59
nm108 <neomahakala108@gmail.com>
2023-07-02 20:19:45 +0200

    prototyping remote database connection. (MSSQL) updated sql query file for Products, instead of Users.

--- app/src/sql/mssqlexpress2017/initDBAW.sql
+++ app/src/sql/mssqlexpress2017/initDBAW.sql
@@ -11,3 +11,20 @@ insert into Users values('Andrzej Wysocki',1020)
 insert into Users values('user1', 3213)
 insert into Users values('user2', 3021)
 select * from Users
+
+drop table Products
+create table Products(
+    ID int(1,1) PRIMARY KEY,
+    ProductName VARCHAR(30) UNIQUE NOT NULL,
+    ProductQuantity int NOT NULL
+)
+select* from Products
+insert into Products(ProductName, ProductQuantity) values("Product #1", 108);
+insert into Products(ProductName, ProductQuantity) values("Product #2", 1080);
+insert into Products(ProductName, ProductQuantity) values("Product #3", 10);
+insert into Products(ProductName, ProductQuantity) values("Product #4", 18);
+insert into Products(ProductName, ProductQuantity) values("Product #5", 18);
+insert into Products(ProductName, ProductQuantity) values("Product #6", 100);
+insert into Products(ProductName, ProductQuantity) values("Product #7", 100);
+insert into Products(ProductName, ProductQuantity) values("Product #8", 100);
+insert into Products(ProductName, ProductQuantity) values("Product #9", 108);
