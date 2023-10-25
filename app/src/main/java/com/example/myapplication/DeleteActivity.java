67b4c5e5482029d1699febb283580ae418bf7959
nm108 <neomahakala108@gmail.com>
2023-10-25 16:04:33 +0200

    updated delete activity to use async task and to connect to free remote database. (https://freeasphosting.net).

--- app/src/main/java/com/example/myapplication/DeleteActivity.java
+++ app/src/main/java/com/example/myapplication/DeleteActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -27,12 +30,28 @@ public class DeleteActivity extends AppCompatActivity {
 
     private AlertDialog dad;
 
+
+    private Context c;
+
+
+    private ProgressDialog pd;
+
+
+    private boolean busy = false;
+
 private EditText deleteQueryEditText;
 
     private ListView lv;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        c = this;
+        pd = new ProgressDialog(this);
+        pd.setCancelable(false);
+        pd.setCanceledOnTouchOutside(false);
+        pd.setMessage("Please Wait.");
+
         setContentView(R.layout.activity_delete);
         doQueryButton = findViewById(R.id.DeleteDoSelectQueryButton);
         deleteQueryEditText = findViewById(R.id.DeleteQueryEditText);
@@ -53,6 +72,15 @@ private EditText deleteQueryEditText;
 
         );
 
+//        doSelectQueryButton.setClickable(true);
+
+        returnButton.setOnClickListener(
+                (final View v) -> {
+                    switchActivities();
+                }
+
+        );
+
 
     }
 
@@ -63,7 +91,6 @@ private EditText deleteQueryEditText;
     }
 
     private void onClick(View v) {
-        doQueryButton.setVisibility(View.GONE);
 
 populateLV();
         dad = new AlertDialog.Builder( this ).create();
@@ -89,6 +116,20 @@ populateLV();
     }
 
     void populateLV() {
+        if (busy) return;
+        busy = true;
+        pd.show();
+
+        DeleteActivity.SQLTask sTask = new DeleteActivity.SQLTask();
+        Integer[] sarr = new Integer[]{};
+        ArrayList<Product> data = new ArrayList<Product>();
+        try {
+            sTask.execute(sarr);
+            // Wait for this worker thread’s notification
+//                    sTask.wait();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         JDBCDatabaseHelper jdbcDatabaseHelper = new JDBCDatabaseHelper();
         ArrayList selectList = null;
@@ -117,6 +158,44 @@ populateLV();
         lv.setAdapter(adapter);
         doQueryButton.setVisibility(View.VISIBLE);
 
+
+
+
+
+     ////        Thread t = new ThreadQuerySQL();
+////        t.run();
+
+//
+
+
     }
+
+
+
+    class SQLTask extends AsyncTask<Integer[], Integer, ArrayList<Product>> {
+
+        public ArrayList<Product> doInBackground(Integer[]... params) {
+            ArrayList<Product> result;
+
+            JDBCDatabaseHelper jdbcDatabaseHelper = new JDBCDatabaseHelper();
+            try {
+                result = jdbcDatabaseHelper.doSelect(deleteQueryEditText.getText().toString());
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Product> products) {
+            super.onPostExecute(products);
+            ProductAdapter pa = new ProductAdapter(c, products);
+            lv.setAdapter(pa);
+            busy = false;
+            pd.dismiss();
+        }
+
+    }
+
 }
 
